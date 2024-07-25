5fa620d3db11225c3179d973ed3f1e76ad2b6f18
Lovtzoff <lovtzoff.aleksei@gmail.com>
2024-07-25 21:35:32 +0300

    Add solutions to task for Lesson_6_testng

--- src/main/java/ru/aston/Main.java
+++ src/main/java/ru/aston/Main.java
@@ -1,7 +1,24 @@
 package ru.aston;
 
+import java.util.stream.LongStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ASTON!");
+        Integer n = 20;
+        System.out.println("Факториал числа " + n + " равен " + getFactorial(n));
+    }
+
+    public static long getFactorial(Integer n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Входные данные должны быть неотрицательным целым числом!");
+        }
+        return LongStream.rangeClosed(1, n)
+                .reduce(1, (long x, long y) -> {
+                    // Проверка на переполнение
+                    if (x > Long.MAX_VALUE / y) {
+                        throw new ArithmeticException("Результат слишком большой для типа long!");
+                    }
+                    return x * y;
+                });
     }
 }
---
+++ src/test/java/ru/aston/MainTest.java
@@ -0,0 +1,68 @@
+package ru.aston;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class MainTest {
+
+    // Тест на случай, когда входное значение равно null
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testGetFactorialWithNullInput() {
+        Main.getFactorial(null);
+    }
+
+    // Метод, который предоставляет параметры для положительных тестов
+    @DataProvider(name = "factorialProvider")
+    public Object[][] factorialProvider() {
+        return new Object[][]{
+                {0, 1},                         // 0! = 1
+                {1, 1},                         // 1! = 1
+                {19, 121645100408832000L},      // 19! = 121645100408832000
+                {20, 2432902008176640000L}      // 20! = 2432902008176640000
+        };
+    }
+
+    @Test(dataProvider = "factorialProvider")
+    public void testGetFactorial(Integer input, long expected) {
+        long result = Main.getFactorial(input);
+        assertEquals(result, expected);
+    }
+
+    // Тест на некорректные входные данные
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testGetFactorialWithNegativeInput() {
+        int input = -1;
+        Main.getFactorial(input); // Исключение будет выброшено
+    }
+
+    // Тест на символы как входные значения
+    @DataProvider(name = "nonIntegerInputProvider")
+    public Object[] nonIntegerInputProvider() {
+        return new Object[]{"a", "@", "1.5"};
+    }
+
+    @Test(dataProvider = "nonIntegerInputProvider", expectedExceptions = NumberFormatException.class)
+    public void testGetFactorialWithNonIntegerInput(String input) {
+        Main.getFactorial(Integer.parseInt(input)); // Попытка преобразовать строку в целое число
+    }
+
+    // Тест на слишком большие значения
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void testGetFactorialWithLargeInput() {
+        int largeInput = 21;
+        Main.getFactorial(largeInput); // Исключение будет выброшено
+    }
+
+    // Метод, который предоставляет параметры для отрицательных тестов (тест на неправильные типы)
+    @DataProvider(name = "wrongTypeInputProvider")
+    public Object[] wrongTypeInputProvider() {
+        return new Object[]{"string", true, 3.14};
+    }
+
+    @Test(dataProvider = "wrongTypeInputProvider", expectedExceptions = ClassCastException.class)
+    public void testGetFactorialWithString(Object input) {
+        Main.getFactorial((Integer) input); // Исключение будет выброшено
+    }
+}
